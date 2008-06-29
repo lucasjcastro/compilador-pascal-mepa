5f99254238a4a27c5db4d7dd3dd5d3252e3190ad
solenoide.spm <solenoide.spm@76a15798-bd50-0410-b6c7-c3f5fcad225f>
2008-06-29 00:27:18 +0000

    Alteração na main para teste da classe hash.

--- trunk/compilador/main.cpp
+++ trunk/compilador/main.cpp
@@ -2,11 +2,34 @@
 #include <fstream>
 #include <string>
 #include "lexico.h"
+#include "hash.h"
 
 int main(int argc, char* argv[]){
-	std::string filename = argv[1];
+	///TESTE DE HASH DAQUI...
+    hash testoe;
+    string palavra1, 
+           palavra2, 
+           palavra3, 
+           palavra4,
+           resultado;
+    
+    palavra1 = "teste de banco";
+    palavra2 = "parametro";
+    
+    palavra3 = "Tudo em ordem?";
+    palavra4 = "variavel";
+    
+    
+    testoe.setHash(palavra1, palavra2);
+    resultado = testoe.getCategoria(palavra1);
+    
+    cout << "Resultado da busca por " << palavra1 << " deu " << resultado << endl;    
+    system("pause");    
 
 
+    /// ATÉ AQUI.
+    std::string filename = argv[1];
+    
 	lexico comp;
 	comp.analisador_lexico(filename);
 	comp.printerror();
