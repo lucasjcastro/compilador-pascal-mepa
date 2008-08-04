71f8d35807d3d2d63e147c1e4cf343235c427cae
solenoide.spm <solenoide.spm@76a15798-bd50-0410-b6c7-c3f5fcad225f>
2008-08-04 06:24:15 +0000

    - Ajustes (tokentype do sintatico trocado de ponteiro para variável normal).

--- compilador/main.cpp
+++ compilador/main.cpp
@@ -9,16 +9,16 @@
 int main(int argc, char* argv[]){
 
     std::string filename = argv[1];
-    
-	lexico comp;
-	//sintatico testoe;
-	
-	comp.analisador_lexico(filename);
+    lexico comp;
+	sintatico testoe;
+			
+    comp.analisador_lexico(filename);
 	//comp.printerror();
-	comp.printlist();
+	//comp.printlist();
 	//comp.printtype();
-	//testoe.loadList(comp.getLista());
-	//testoe.analisador_sintatico();
+	
+    testoe.loadList(comp.getLista());
+	testoe.analisador_sintatico();
 	
 	return 0;
 }
